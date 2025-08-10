6d11766411030e00858e1ec838c4ab9cc8b5fb7d
mr-kunal-07 <kunaljadhav2305@gmail.com>
2025-08-11 03:00:18 +0530

    KodX

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -1,8 +1,13 @@
+import Contact from '@/components/LandingPage/Contact'
+import WhatWeDo from '@/components/LandingPage/WhatWeDo'
 import React from 'react'
 
 const page = () => {
   return (
-    <div className='w-full min-h-screen'>Services</div>
+    <div className='w-full min-h-screen'>
+      <WhatWeDo/>
+      <Contact/>
+    </div>
   )
 }
 
